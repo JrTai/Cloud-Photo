f02315987fb4189396e72981c4592c9ffc860bbd
Jr-Han Tai <photoniclaser@gmail.com>
2021-05-19 19:55:02 +0800

    add each photo size to photo table

--- server/controllers/upload_controller.js
+++ server/controllers/upload_controller.js
@@ -6,13 +6,14 @@ const createUserPhoto = async (req, res) => {
   let uploadSize = 0;
   const userid = 1;
   let queryStr1 =
-    "INSERT INTO photo (userid, url, albumid, public, trash, date, diaryid) VALUES ";
+    "INSERT INTO photo (userid, url, albumid, public, trash, date, diaryid, size) VALUES ";
   for (let i = 0; i < req.files.length; i += 1) {
     // orders insert
     const unixTime = parseInt(req.files[i].key.split(".")[0]);
     const date = moment(unixTime).format("YYYY-MM-DDTHH:mm:ss.SSS");
-    queryStr1 += `(${userid}, "${req.files[i].location}", null, false, false, "${date}", null),`; // userid need to be flex
-    uploadSize += req.files[i].size;
+    const size = req.files[i].size;
+    queryStr1 += `(${userid}, "${req.files[i].location}", null, false, false, "${date}", null, ${size}),`; // userid need to be flex
+    uploadSize += size;
   }
   queryStr1 = queryStr1.replace(/.$/, ";");
   await Database.queryDB(queryStr1);
